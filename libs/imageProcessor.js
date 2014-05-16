c0cde22d3ae6c3afb7be2f681527ad57e6c9b807
Vincent Peybernes <vpeybernes.pro@gmail.com>
2014-05-17 00:23:05 +0200

    Implement watcher.

--- libs/imageProcessor.js
+++ libs/imageProcessor.js
@@ -13,15 +13,15 @@ var extReg  = /\.([a-zA-Z0-9]+)$/;
 var sizeReg = /([0-9]+)x([0-9]+)/;
 
 var galleryPath = config.galleryPath    = path.resolve(__dirname+"/..", path.join(config.publicDirectory, config.gallery));
-var thumbsPath   = config.thumbPath      = path.resolve(galleryPath, "thumbs");
+var thumbsPath   = config.thumbPath     = path.resolve(galleryPath, "thumbs");
 var dataPath    = config.dataPath       = path.resolve(galleryPath, "data");
 
 
 processGallery();
 
-//fs.watch(galleryPath, function(event, file){
-//
-//});
+fs.watch(galleryPath, function(event, file){
+    processFile(file);
+});
 
 function createThumb(filename){
     var filePath    = path.join(galleryPath, filename);
@@ -45,11 +45,12 @@ function createDataUrl(filename, extention){
 function processGallery(){
     console.log("Gallery processing");
 
-    if(!fs.existsSync(galleryPath)) throw new Error("The gallery path not exist.");
+    if(!fs.existsSync(galleryPath)) fs.mkdirSync(galleryPath);
     if(!fs.statSync(galleryPath).isDirectory()) throw new Error("The gallery path is not a directory.");
-
-    if(!fs.existsSync(thumbsPath))   fs.mkdirSync(thumbsPath);
-    if(!fs.existsSync(dataPath))    fs.mkdirSync(dataPath);
+    if(!fs.existsSync(thumbsPath)) fs.mkdirSync(thumbsPath);
+    if(!fs.statSync(thumbsPath).isDirectory()) throw new Error("The thumb path in the gallery is not a directory.");
+    if(!fs.existsSync(dataPath)) fs.mkdirSync(dataPath);
+    if(!fs.statSync(dataPath).isDirectory()) throw new Error("The data path in the gallery is not a directory.");
 
     if(typeof config.thumbnailSize == "string"){
         if(!sizeReg.test(config.thumbnailSize)) throw new Error("Invalid thumbnailSize parameter. It must be like 400x400");
@@ -61,13 +62,18 @@ function processGallery(){
 
     var files = fs.readdirSync(galleryPath);
     for( var i in files ){
-        var filename = files[i];
-        if(extReg.test(filename) && config.allowExtension[RegExp.$1]){
-            var extention = RegExp.$1;
-            stdout.write("    Process "+filename+": ");
-            createDataUrl(filename, extention);
-            if(!config.disabledThumbnail) createThumb(filename);
-            stdout.write("OK!\n".green);
-        }
+        processFile(files[i]);
+    }
+
+    console.log("--------------");
+}
+
+function processFile(filename){
+    if(extReg.test(filename) && config.allowExtension[RegExp.$1]){
+        var extention = RegExp.$1;
+        stdout.write("    Process "+filename+": ");
+        createDataUrl(filename, extention);
+        if(!config.disabledThumbnail) createThumb(filename);
+        stdout.write("OK!\n".green);
     }
 }
